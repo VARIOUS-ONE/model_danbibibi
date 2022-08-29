ecf9d0504715100e88ba722a9f908c0b9b78ef96
LIMDANBI <dlaeksql08222@gmail.com>
2022-08-29 16:16:36 +0900

    return 부정 0 / 긍정 1

--- predict.py
+++ predict.py
@@ -14,8 +14,8 @@ from kobert.utils import get_tokenizer
 from kobert.pytorch_kobert import get_pytorch_kobert_model
 
 # parameter 
-max_len = 64
-batch_size = 64
+max_len = 128
+batch_size = 32
 
 # device 
 device = torch.device('cpu')
@@ -40,6 +40,8 @@ def predict(predict_sentence):
 
     another_test = BERTDataset(dataset_another, 0, 1, tok, max_len, True, False)
     test_loader = torch.utils.data.DataLoader(another_test, batch_size=batch_size, num_workers=0)
+    
+    model.eval()
 
     for batch_id, (token_ids, valid_length, segment_ids, label) in enumerate(test_loader):
         token_ids = token_ids.long().to(device)
@@ -49,25 +51,17 @@ def predict(predict_sentence):
         label = label.long().to(device)
 
         out = model(token_ids, valid_length, segment_ids)
-
-        test_eval=[]
+        
         for i in out:
             logits=i
             logits = logits.detach().cpu().numpy()
 
-            if np.argmax(logits) == 0:
-                test_eval.append("부정적")
-            else:
-                test_eval.append("긍정적")
+            return np.argmax(logits)
 
-        print(">> 해당 리뷰는 " + test_eval[0] + " 리뷰 입니다.")
-
-print("\n0을 입력하면 리뷰 감성분석 프로그램이 중단됩니다.\n")
-
-while True:
-    sentence = input("긍부정을 판단할 리뷰를 입력해주세요 : ")
-    if sentence == "0":
-      print(">> 긍부정 판단을 종료합니다!\n")
-      break
-    predict(sentence)
-    print("\n")
+# while True:
+#     sentence = input("긍부정을 판단할 리뷰를 입력해주세요 : ")
+#     if sentence == "0":
+#       print(">> 긍부정 판단을 종료합니다!\n")
+#       break
+#     print(predict(sentence))
+#     print("\n")
